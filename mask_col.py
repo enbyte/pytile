6596d585ef958c4a16426af0ea1749aa620bec6c
Sunny Crowder-Sklar <felixpodzol@gmail.com>
2021-05-08 11:00:26 -0400

    added _convert_tf to mask col

--- mask_col.py
+++ mask_col.py
@@ -1,2 +1,16 @@
+def _convert_tf(d, includes=[0]):
+    assert d.keys() == [1,2,3,4]
+    for key in d.keys():
+        x = d[key]
+        if not x in includes:
+            d[key] = False
+        else:
+            d[key] = True
+            
+def _get_around(matrix, row, col):
+    build = {1:None, 2:None, 3:None, 4:None}
+    
+
+
 def genColMask(*args, **kwargs):
 	pass
